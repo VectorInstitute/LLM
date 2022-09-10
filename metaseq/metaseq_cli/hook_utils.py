c16c29a10ba314fe766a3a841ce43450ead345c0
Gerald Shen <gerald.shen@mail.utoronto.ca>
2022-09-10 01:47:23 -0400

    most shapes correct but need to deal with attn head split

--- metaseq/metaseq_cli/hook_utils.py
+++ metaseq/metaseq_cli/hook_utils.py
@@ -1,8 +1,12 @@
 import torch
+from einops import rearrange
 
 from megatron.mpu.mappings import gather_from_tensor_model_parallel_region
 
 from megatron.mpu import ColumnParallelLinear, RowParallelLinear
+
+from metaseq.modules.layer_norm import FusedLayerNorm
+from metaseq.modules.dropout import Dropout
 
 from metaseq.model_parallel.modules.transformer_layer import (
     ModelParallelTransformerDecoderLayer,
@@ -56,25 +60,51 @@ def forward_hook_fn(registered_name, save_dict, m, _, outputs):
     # NOTE: don't touch the inputs! THEY MIGHT BE WRONG
     # the inputs to the row parallel layers
     # are sharded, so we need to unshard them
-
-    # only save it on rank 0 but i think we need to call the gather in all
-    # processes so this hook needs to be installed on every process
     type_m = type(m)
+
+    # every rank needs to do this
+    if type_m == ColumnParallelLinear:
+        if not m.gather_output:
+            outputs = (
+                gather_from_tensor_model_parallel_region(outputs[0]),
+                *outputs[1:],
+            )
+
+    # only rank 0 needs to do the rest
+    if torch.distributed.get_rank() != 0:
+        return
 
     # too scared to do isinstance checks
     if type_m == ColumnParallelLinear:
 
-        if not m.gather_output:
-            output = gather_from_tensor_model_parallel_region(outputs[0])
-
-        if m.skip_bias_add:
-            output = output + outputs[1]
-
-    elif type_m == RowParallelLinear:
         output = outputs[0]
 
         if m.skip_bias_add:
             output = output + outputs[1]
+
+        layer_type = registered_name.split(".")[-1]
+
+        # not always S x B x D, it's only when it's used in qkv_proj
+        if layer_type == "qkv_proj":
+            output = rearrange(output, "s b d -> b s d")
+        elif "fc" in layer_type:
+            output = rearrange(output, "(s b) d -> b s d", b=save_dict["_batch_size"])
+
+    elif type_m == RowParallelLinear:
+
+        output = outputs[0]
+
+        if m.skip_bias_add:
+            output = output + outputs[1]
+
+        layer_type = registered_name.split(".")[-1]
+
+        # not always S x B x D, it's only when it's used in outproj
+        if layer_type == "out_proj":
+            output = rearrange(output, "s b d -> b s d")
+
+        elif "fc" in layer_type:
+            output = rearrange(output, "(s b) d -> b s d", b=save_dict["_batch_size"])
 
     elif type_m in (
         ModelParallelTransformerDecoder,
@@ -90,11 +120,26 @@ def forward_hook_fn(registered_name, save_dict, m, _, outputs):
         if attn_bias is not None:
             output = output + attn_bias
 
+    elif type_m == Dropout:
+        output = outputs
+
+        if "self_attn" in registered_name:
+            ...
+        else:
+            output = rearrange(output, "s b d -> b s d")
+
     # best effort kind of thing
     else:
         # works on VocabParallelEmbedding
         output = outputs
 
-    if torch.distributed.get_rank() == 0:
-        # only pytorch tensors allowed for now
-        save_dict[registered_name] = output.detach().cpu()
+    # some layers are always in S x B x D, permute them back
+    if type_m in (
+        ModelParallelTransformerDecoderLayer,
+        FusedLayerNorm,
+        ModelParallelMultiheadAttention,
+    ):
+        output = rearrange(output, "s b d -> b s d")
+
+    # only pytorch tensors allowed for now
+    save_dict[registered_name] = output.detach().cpu()
